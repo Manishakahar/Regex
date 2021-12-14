f91f661ae596a6926c68b6eb3ac080a4cd4acf9b
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-14 21:25:46 +0530

    Added:check PIN code from alphabets is fail

--- src/com/blz/regex/PinCodeRegex.java
+++ src/com/blz/regex/PinCodeRegex.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class PinCodeRegex {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Program");
-        String pinCode = "400088";
-        String regex = "^[0-9]{6}$";
+        String pinCode = "A400088";
+        String regex = "^[1-9]{1}[0-9]{5}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(pinCode);
         if (matcher.matches()){
