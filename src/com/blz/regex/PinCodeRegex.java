a850870b26aacf5762abf654be01908ee6d6b9fe
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-14 22:21:16 +0530

    Added: Code to handel sapce between pincode.

--- src/com/blz/regex/PinCodeRegex.java
+++ src/com/blz/regex/PinCodeRegex.java
@@ -6,8 +6,9 @@ import java.util.regex.Pattern;
 public class PinCodeRegex {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Program");
-        String pinCode = "400088B";
-        String regex = "^[1-9]{1}[0-9]{5}![A-Z,a-z]$";
+        String pinCode = "400 088";
+        String regex = "^[1-9]{1}[0-9]{2} {0,1}[0-9]{3}$";
+
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(pinCode);
         if (matcher.matches()){
