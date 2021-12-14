bde1d14b00e47e5d2c0ae222ed31096af1995a84
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-14 21:13:52 +0530

    Added: check pin code is valid or not

--- src/com/blz/regex/PinCodeRegex.java
+++ src/com/blz/regex/PinCodeRegex.java
@@ -1,8 +1,23 @@
 package com.blz.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PinCodeRegex {
     public static void main(String[] args) {
         System.out.println("Welcome to Regex Program");
+        String pinCode = "400088";
+        String regex = "^[0-9]{6}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pinCode);
+        if (matcher.matches()){
+            System.out.println("Pin code is valid");
+        }
+        else {
+            System.out.println("Enter the valid Pin code");
+        }
+
     }
 
 }
+
